4f5731484f8300ea06abbce5845ab670e4349026
Stephen Quan <squan@esri.com>
2023-06-07 17:13:46 +1000

    Implement TicTacToe (basic)

---
+++ TicTacToe/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace TicTacToe;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void OnNavigate(object sender, EventArgs e)
+	{
+		Button btn = (Button)sender;
+		string arg = btn.CommandParameter.ToString();
+		await Shell.Current.GoToAsync(arg);
+	}
+}
+
---
+++ TicTacToe/Pages/AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace TicTacToe.Pages;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
