c975941c38aa6e9bb29a0700c42eb7553f5d8641
Stephen Quan <squan@esri.com>
2023-06-20 13:01:44 +1000

    Add TicTacToe-intermediate

---
+++ TicTacToe-intermediate/AppShell.xaml.cs
@@ -0,0 +1,14 @@
+﻿using TicTacToe.Pages;
+
+namespace TicTacToe;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+
+		Routing.RegisterRoute(nameof(AboutPage), typeof(AboutPage));
+		Routing.RegisterRoute(nameof(GamePage), typeof(GamePage));
+	}
+}
---
+++ TicTacToe-intermediate/Pages/GamePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace TicTacToe.Pages;
+
+public partial class GamePage : ContentPage
+{
+	public GamePage()
+	{
+		InitializeComponent();
+	}
+}
